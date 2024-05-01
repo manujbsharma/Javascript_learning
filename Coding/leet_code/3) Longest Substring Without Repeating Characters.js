3b901ad7f391d07a6b0d79862190a3371edb19c7
Manuj Bandhu Sharma <dr.dacron@gmail.com>
2024-05-01 12:47:48 +0530

    solved using Array and its methods or by map

--- Coding/leet_code/3) Longest Substring Without Repeating Characters.js
+++ Coding/leet_code/3) Longest Substring Without Repeating Characters.js
@@ -7,7 +7,6 @@ let s = "abcabcbb"
 //  ----------------------------------------------------------------------------------------------//
 //        To copy value of one variable to another variable (not refering to other variable)      //
 //------------------------------------------------------------------------------------------------//
-// // Function to assign a value of one variable to another variable 
 // function copy(x) {
 //     return JSON.parse( JSON.stringify(x) );
 // }
@@ -17,14 +16,14 @@ let s = "abcabcbb"
 //------------------------------------------------------------------------------------------------//
 
 
-let subStr = new Array() , reqStr = new Array(); // initiating 2 arrays
+let subSection = new Array() , reqStr = new Array(); // initiating 2 arrays
 
 for (let i = 0; i < s.length; i++) {  // for each character of the given string
     
-    let reqIndex = subStr.indexOf(s[i]); // saving the index of current character from string 
+    let reqIndex = subSection.indexOf(s[i]); // saving the index of current character from string 
     
     if (reqIndex > -1) { // if the character is present in subStr array
-        subStr.splice(0, reqIndex+1) // slice the elements till [the index of the character we are looking for] 
+        subSection.splice(0, reqIndex+1) // slice the elements till [the index of the character we are looking for] 
         /*
         Suppose, subStr = ['c','e','l','p','h','o','n','e']
         if we want to remove ['c','e','l'] --from--> ['c','e','l','p','h','o','n','e']  => ['p','h','o','n','e'] the result we want
@@ -33,16 +32,16 @@ for (let i = 0; i < s.length; i++) {  // for each character of the given string
         */
     }
 
-    subStr.push(s[i]) // insert the current character in the end of the subStr array
+    subSection.push(s[i]) // insert the current character in the end of the subStr array
 
-    if (reqStr.length < subStr.length) { // if length of reqStr Array(required string) is smaller than subStr Array(sub string)
+    if (reqStr.length < subSection.length) { // if length of reqStr Array(required string) is smaller than subStr Array(sub string)
         // reqStr = copy(subStr) // Another way to copy.... any value from one variable to another variable [function written above]
-    reqStr = subStr.slice(0) // then copy the values from subStr Array to reqStr Array
+    reqStr = subSection.slice(0) // then copy the values from subStr Array to reqStr Array
 }
 }
 console.log(`First longest sub string in string: "${s}" is: ${reqStr.join("")}`)
 console.log(`The Length od sub string is: ${reqStr.length}`)
-return reqStr.length
+// return reqStr.length
 
 
 // -----------------------------------------------------------------------------------------------------------------------------------
@@ -72,5 +71,29 @@ for (let i = 0; i < s.length; i++) {  // for each character of the given string
 
     reqLen = Math.max(subStr.length,reqLen) // storing the longest substring length
 }
-// console.log(`longest substring length is: ${reqLen}`)
-return reqLen
+console.log(`longest substring length is: ${reqLen}`)
+// return reqLen
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------------//
+//             If you want to print the length of longest substring only --Using MAP              //
+//------------------------------------------------------------------------------------------------//
+
+
+let x = 0;
+let len = 0;
+const map = new Map();
+
+for (let j=0;j<s.length;j++) {
+    const index = map.get(s[j]);
+    
+    if(index !== undefined && index >= x) {
+        x = index + 1;
+    }
+    
+    len = Math.max(len, j-x+1);
+    map.set(s[j], j);
+}
+
+console.log(len);
