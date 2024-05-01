4efad80e3c02994ed8deae6953978e6adb366259
Manuj Bandhu Sharma <dr.dacron@gmail.com>
2024-05-01 20:20:15 +0530

    solved using sliding window approach and map functions

---
+++ Coding/leet_code/340) Longest Substring with At Most K Distinct Characters.js
@@ -0,0 +1,49 @@
+s = "aaaaaaabbbaaaacdcdcdcdcdcdcdcdcdcdcdcdcdcc"
+distinctCharacters = 3
+// "aaaaaaabbbaaaacc"
+// "eceba"
+
+if (s.length < distinctCharacters+1) { // if the string size is less than desired Distinct Characters 
+    console.log(`length of the string: ${s} is ${s.length}`)
+    return s.length // return its length (the code below won't run)
+}
+
+
+let cnt = new Map(); // initiated a new map object to store the values (between the start pointer and end pointer)
+let startPoint = 0; // starting pointer 
+let max = 0 // storing the maximum length
+let resultantStr = ""; // (optional) to store the desired longest sub-string
+
+for (let endPoint = 0; endPoint < s.length; endPoint++) { // starting the end pointer
+    
+    if (!cnt.get(s[endPoint])) { cnt.set(s[endPoint], 1) } // If the current letter doesn't exist in Map, add current letter inside the Map with count as 1 --> {e:1} at s[0] 
+    else { cnt.set(s[endPoint], cnt.get(s[endPoint]) + 1) } // if current letter exists, increase the count of existing value by 1
+
+    while (cnt.size > distinctCharacters) { // while the size of Map is more than desired Distinct Characters 
+
+        if (cnt.get(s[startPoint]) > 1) { // if the value is more than 1 for the letter at starting pointer index(initially 0) (from s string). 
+            cnt.set(s[startPoint], cnt.get(s[startPoint]) - 1) // decrease its count by 1 
+        }
+        else {
+            cnt.delete(s[startPoint])// else if the count is 1, delete the letter
+        }
+        startPoint++ // increase the value of start pointer by 1
+
+        /* THE LOGIC FOR SLIDING WINDOW 
+         * This while loop works as a sliding window to maintain the size of Map as 2.
+         *  if size of map becomes more than desired Distinct Characters . (if it holds more than desired Distinct Characters at a time ) 
+         * It will remove the letter (if its count is 1) OR decrease the count of letter (at starting pointer index from s string)
+         */
+    }
+
+    if ((endPoint - startPoint) + 1 > max) { // if the number of letter are more than the value in max variable....
+        resultantStr = s.slice(startPoint, endPoint + 1) // place the sub string inside the resultantStr variable
+    }
+
+    max = Math.max(max, (endPoint - startPoint) + 1) // update the value in max variable by comparing the maximum value between [(number of letters of substring) & current value of max variable ]
+}
+
+console.log(max)
+console.log(resultantStr)
+
+return max;
